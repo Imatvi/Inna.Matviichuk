8da98cd462b5a34c9d16d5ec6ca00f69993d1e04
Inna Matviichuk <imatvi@softserveinc.com>
2022-11-30 23:35:37 +0200

    Commit tasks

--- HomeWork1/index.js
+++ HomeWork1/index.js
@@ -1,3 +1,90 @@
-console.log("das ist fantastisch");
-let message = "well well well";
-console.log(message);
+// console.log("das ist fantastisch");
+// let message = "well well well";
+// console.log(message);
+// const person = {
+//     name: "Wednesday",
+//     sex: 'female',
+//     age: 14
+// };
+// console.log(person);
+// let button = "Enter";
+// console.log(typeof button);
+
+// let age = prompt ("How old are you?", "18");
+// alert(age);
+
+// let agree = confirm("Are you agree?");
+// alert(agree);
+
+// task2
+alert('Matviichuk');
+
+// task3
+let lightSource1 = 'candel';
+let lightSource2 = 'lantern';
+console.log(lightSource1, lightSource2);
+lightSource1 = lightSource2;
+console.log(lightSource1, lightSource2);
+
+// task4
+const person = {
+name1: 'Hulk', //string
+age: 43, //number
+isStrong: true, //boolean
+senseOfHumor: "", //undefined
+stressResistance: "" //null
+};
+console.log(typeof name1);
+console.log(typeof age);
+console.log(typeof strong);
+console.log(typeof senseOfHumor);
+console.log(typeof stressResistance);
+
+// task5
+
+let isAdult = confirm("Are you over 18 years old?");
+alert(isAdult);
+
+// task6
+let name2;
+let lastName;
+let group;
+let birthYear;
+
+name2 = "Inna"; //string
+lastName = "Matviichuk"; //string
+group = "LV-732"; //string
+birthYear = 1990; //number
+
+let notMarried = true; //boolean
+
+console.log(birthYear, notMarried, name2, lastName, group);
+
+let programmer;
+let JSexpert = null;
+console.log(typeof programmer);
+console.log(typeof JSexpert);
+
+
+// task7
+let name3 = prompt ("Please enter your name", "User");
+let email = prompt ("Please enter your email address", "usermale@gmail.com");
+let password = prompt ("Please enter your password for emnail", "qwerty");
+
+console.log("Dear " + name3 + ", your email is " + email + ", your password is " + password);
+alert("Dear " + name3 + ", your email is " + email + ", your password is " + password);
+
+// task8
+
+let secondsInHour;
+let secondsInDate;
+let secondsInMounth;
+let daysInMounth;
+
+secondsInHour = 60 * 60;
+secondsInDate = secondsInHour * 24;
+secondsInMounth = daysInMounth * secondsInDate;
+
+console.log(secondsInHour, secondsInDate, secondsInMounth);
+
+
