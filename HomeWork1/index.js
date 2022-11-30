1c0865252fc87c7b1b293900d144312400af0414
Inna Matviichuk <imatvi@softserveinc.com>
2022-11-30 23:39:39 +0200

    Add drafts

--- HomeWork1/index.js
+++ HomeWork1/index.js
@@ -1,21 +1,3 @@
-// console.log("das ist fantastisch");
-// let message = "well well well";
-// console.log(message);
-// const person = {
-//     name: "Wednesday",
-//     sex: 'female',
-//     age: 14
-// };
-// console.log(person);
-// let button = "Enter";
-// console.log(typeof button);
-
-// let age = prompt ("How old are you?", "18");
-// alert(age);
-
-// let agree = confirm("Are you agree?");
-// alert(agree);
-
 // task2
 alert('Matviichuk');
 
