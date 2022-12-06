63bd38fbfef33be09dfcedfc25c72945fac3f8b7
Inna Matviichuk <imatvi@softserveinc.com>
2022-12-06 18:56:46 +0200

    Add changes

--- Drafts/drafts.js
+++ Drafts/drafts.js
@@ -1,17 +1,26 @@
-console.log("das ist fantastisch");
-let message = "well well well";
-console.log(message);
-const person = {
-    name: "Wednesday",
-    sex: 'female',
-    age: 14
-};
-console.log(person);
-let button = "Enter";
-console.log(typeof button);
+// console.log("das ist fantastisch");
+// let message = "well well well";
+// console.log(message);
+// const person = {
+//     name: "Wednesday",
+//     sex: 'female',
+//     age: 14
+// };
+// console.log(person);
+// let button = "Enter";
+// console.log(typeof button);
 
-let age = prompt ("How old are you?", "18");
-alert(age);
+// let age = prompt ("How old are you?", "18");
+// alert(age);
 
-let agree = confirm("Are you agree?");
-alert(agree);
+// let agree = confirm("Are you agree?");
+// alert(agree);
+
+let a = -11;
+if (a > 0) {
+   console.log("Number " + a + " positive");
+} else if (a < 0) {
+   console.log("Number " + a + " negative");
+} else {
+   console.log("Number " + a + " is exactly zero");
+}
--- HomeWork2/index.js
+++ HomeWork2/index.js
@@ -1,4 +1,4 @@
-//task1
+//task1: Допишіть код, щоб в консолі браузера з'явилися рядки, які написані в коментарях, оперуючи при цьому змінними х і у.
 let x = 1;
 let y = 2;
 
@@ -21,13 +21,83 @@ console.log(res4); // NaN
 console.log(typeof res4); // "number"
 
 //task2
+//Користувач вводить число (використовуйте prompt ()). Необхідно перевірити чи є введене число
+// 	a) парним додатним;
+// 	b) кратним числу 7.
+// Результат перевірки вивести в консоль
+
 let number = prompt("Please enter any number");
 let result1 = number > 0; 
 let result2 = (number % 2 == 0);
-//let result3 = (number = Math.sqrt(7));
-let result3 = (number (Math.sqrt(7)));
+let result3 = (number % 7 == 0);
 
 console.log(result1);
 console.log(result2);
 console.log(result3);
 
+if (result1 = number > 0) {
+    console.log(number + " is positive.");
+} else if (result1 = number < 0) {
+    console.log(number + " is negative.");
+};
+
+if (result2 = (number % 2 == 0)) {
+    console.log(number + " is even.");
+} else if (result2 = (number % 2 != 0)) {
+    console.log(number + " is odd.");
+};
+
+if (result3 = (number % 7 == 0)) {
+    console.log(number + " is a multiple of seven.")
+} else if ((number % 7 != 0)) {console.log(number + " is not a multiple of seven.");
+
+};
+
+//task3: Створіть порожній масив
+let array1 = [];
+
+array1[0] = 5; //У перший елемент масиву запишіть будь-яке число
+console.log(array1[0]);
+
+array1[1] = "submarines"; //У другий елемент масиву запишіть будь-який рядок;
+console.log(array1[1]);
+
+array1[2] = 1 > 0; //У третій елемент масиву запишіть будь-яке логічне значення;
+console.log(array1[2]);
+
+array1[3] = null; //У четвертий елемент масиву запишіть значення null;
+console.log(array1[3]);
+
+console.log(array1.length); //Виведіть на екран число елементів, яке зберігається в масиві;
+
+array1[4] = prompt("Please enter any value"); //Запитайте у користувача ввести будь-яке значення і запишіть це значення в п'ятий елемент масиву;
+
+console.log(array1[4]); //Виведіть на екран п'ятий елемент масиву.
+
+
+array1.shift(); //Видаліть 1-ий елемент масиву і виведіть масив на екран.
+
+console.log(array1);
+
+
+//task4: Напишіть програму, яка об’єднує всі наявні елементи масиву в стрічку і розділяє їх символом “*”.
+
+const cities = ["Rome", "Lviv", "Warsaw"];
+
+console.log(cities[0] + "*" + cities[1] + "*" + cities[2]);
+
+//task5: Використовуючи функцію prompt() задайте користувачу питання про досягнення ним повнолітнього віку. Результат запишіть в змінну isAdult. В залежності від отриманого значення виведіть відповідне повідомлення про статус особи. Наприклад: при виконанні умови вік більше 18 вивести “Ви досягли повнолітнього віку”. Якщо вік менше 18 років вивести “Ви ще надто молоді”, 
+
+let isAdult1 = prompt ("Скільки вам років?");
+
+if (isAdult1 > 18 ) {
+    console.log("Ви досягли повнолітнього віку.");
+} else if (isAdult1 < 18 ) {
+    console.log("Ви ще надто молоді.");
+};
+
+// if (isAdult1 > 18 ) {
+//     alert("Ви досягли повнолітнього віку.");
+// } else if (isAdult1 < 18 ) {
+//     alert("Ви ще надто молоді.");
+// };
