efe00e1c30182e668f40cd00bf2eef49393bd344
Inna Matviichuk <imatvi@softserveinc.com>
2022-12-29 18:45:42 +0200

    Add changes

--- Drafts2/drafts2.js
+++ Drafts2/drafts2.js
@@ -13,32 +13,89 @@
 //     console.log(element1);
 // }
 
-let numbers = [1, 2, 3, 4, 5 , 6, 7];
+// let numbers = [1, 2, 3, 4, 5 , 6, 7];
 
-for(let element2 of numbers) {
-    if(element2 === 4) {
-        break;
-    }
-    console.log(element2);
-}
+// for(let element2 of numbers) {
+//     if(element2 === 4) {
+//         break;
+//     }
+//     console.log(element2);
+// }
 
-for (let element3 of numbers) {
-    if(element3 === 4 || element3 === 6) {
-        continue;
-    }
-    console.log(element3);
-}
+// for (let element3 of numbers) {
+//     if(element3 === 4 || element3 === 6) {
+//         continue;
+//     }
+//     console.log(element3);
+// }
 
-for(element4 of numbers) {
-    if(element4 % 2 === 0) {
-        continue;
-    }
-    console.log(element4);
-}
+// for(element4 of numbers) {
+//     if(element4 % 2 === 0) {
+//         continue;
+//     }
+//     console.log(element4);
+// }
 
-function sendMessage() {
-    alert("My message to the world!");
-}
+// function sendMessage() {
+//     alert("My message to the world!");
+// }
 
-sendMessage();
-console.log(sendMessage);
+// sendMessage();
+// console.log(sendMessage);
+
+// function multiplication(a, b) {
+//     let result = a * b;
+//     console.log(result);
+// }
+
+// multiplication(15, 55);
+// multiplication(556, 1254);
+
+// console.log("multiplication1")
+// function multiplication1 (x, y, z) {
+//     if (y === undefined) {
+//         y = 1;
+//     }
+//     if (z === undefined) {
+//         z = 2;
+//     }
+//     let result1 = x * y * z;
+//     console.log(result1);
+// }
+
+// multiplication1(2);
+// multiplication1(2, 2);
+
+// function someFunc() {
+//     console.log(arguments);
+// }
+// someFunc(5, 4, 4, 6, 4)
+
+// function multiplication2() {
+//     let result2 = 1;
+//     for (let i = 0; i < arguments.length; i++) {
+//         result2 *= arguments[i];
+//     }
+//     console.log(result2);
+// }
+
+// multiplication2(3, 15, 5, 5);
+
+// function multiplication(a, b) {
+//     let result = a * b;
+//     return result;
+// }
+
+// console.log(multiplication(5, 9));
+
+// function sayHello() {
+//     console.log("Hi!")
+// }
+
+// let hello = sayHello;
+// hello();
+// console.log(hello());
+
+// let sendMessage = function() {alert("Hoi!");};
+
+ function
