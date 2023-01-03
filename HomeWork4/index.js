3d7e3e018c80ac6f3e49be935533542a6b839bb9
Inna Matviichuk <imatvi@softserveinc.com>
2023-01-03 11:58:19 +0200

    Add HW4

---
+++ HomeWork4/index.js
@@ -0,0 +1,18 @@
+console.log("Task#1");
+
+function calcRectangleArea(width, height) {
+    let area = (width * height) / 2;
+    console.log(typeof width);
+    console.log(typeof height);
+    console.log(area);
+    return(area);
+}
+
+calcRectangleArea(10, 8);
+
+
+console.log("Task#2");
+
+console.log("Task#3");
+
+console.log("Task#4");
