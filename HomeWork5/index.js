d5ae70af9997c144540283bc62586b2dfad1abb0
Inna Matviichuk <imatvi@softserveinc.com>
2023-01-11 00:08:14 +0200

    Add changes

---
+++ HomeWork5/index.js
@@ -0,0 +1,27 @@
+//Task#1:
+console.log("Task#1");
+
+function propsCount(currentObject) {
+    let currentObject = {
+    };
+}
+
+let worker = {
+    age:35,
+    hight: 175,
+    weigh: 80
+}
+
+console.log(propsCount(worker));
+
+//Task#2:
+console.log("Task#2");
+
+//Task#3:
+console.log("Task#3");
+
+//Task#4:
+console.log("Task#4");
+
+//Task#5:
+console.log("Task#5");
