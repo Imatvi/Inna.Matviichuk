df90e092cf67bcbcdde540369f5b0242044c295d
Inna Matviichuk <imatvi@softserveinc.com>
2023-01-12 13:19:59 +0200

    Add changes

---
+++ HomeWork6/index.js
@@ -0,0 +1,15 @@
+//Task#1
+console.log("Task#1")
+function defineLetter(str) {
+    let RegExp = /[A-Z]/;
+    if RegExp = true {
+        console.log("String includes upper letters");
+    } else {
+        console.log("String does not includes upper letters");
+    }
+}
+
+let str1 = "uppercase";
+let str2 = "UpperCase";
+console.log(regExp.test(str1));
+console.log(regExp.test(str2)); 
