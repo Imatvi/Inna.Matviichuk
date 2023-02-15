2b9279fba8a64393e65f976313eca417e3b2507a
Inna Matviichuk <imatvi@softserveinc.com>
2023-02-15 12:42:24 +0200

    Add changes

--- Lessons/Intro_1/index.js
+++ Lessons/Intro_1/index.js
@@ -2,3 +2,22 @@ console.log("dobryy den everybody!");
 
 let message = "hello";
 console.log(message);
+
+let house = {
+    name: "House",
+    street: "Green",
+    number: 8
+}
+
+console.log(house);
+
+
+let something;
+console.log("Type of something is " + typeof something);
+
+alert("Hello!");
+
+let name = prompt("What is your name?")
+alert("Hello, " + name + "!");
+
+let name1 = prompt("What is your name?", "Somebody");
