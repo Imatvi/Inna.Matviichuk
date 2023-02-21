d9f27563dd6a9ac9251d5a1c9ce9cf76676b7fe7
Inna Matviichuk <inna.matviichuk@Henryscheinone.com>
2023-02-21 12:10:54 +0200

    Add changes

---
+++ HomeWork10_Jest/1.js
@@ -0,0 +1,1 @@
+getPromise(message, delay)
--- HomeWork6/index.js
+++ HomeWork6/index.js
@@ -13,3 +13,15 @@ let str1 = "uppercase";
 let str2 = "UpperCase";
 console.log(regExp.test(str1));
 console.log(regExp.test(str2)); 
+
+//Task#2:
+console.log("Task#2");
+
+//Task#3:
+console.log("Task#3");
+
+//Task#4:
+console.log("Task#4");
+
+//Task#5:
+console.log("Task#5");
